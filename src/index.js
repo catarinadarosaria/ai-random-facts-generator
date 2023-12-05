d5f16be6af6e7c219c063594609e583e75195fb0
Catarina da Rosária <131613536+catarinadarosaria@users.noreply.github.com>
2023-12-05 20:10:22 +0100

    added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generateFact(event) {
-  event.preventDefault();
+function displayFact(response) {
   new Typewriter("#fact", {
-    strings:
-      "The shortest war in history was between Britain and Zanzibar on August 27, 1896. Zanzibar surrendered after 38 minutes.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+  let input = document.querySelector("#instructions");
+  let apiKey = "84d76171da4e217dad7t0o3ca0d5af7b";
+  let context =
+    "You are an expert in random facts. Your mission is to write a one phrase random fact. Be sure to follow the user instructions.";
+  let prompt = `User instructions: Write a short random fact about ${input.value}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayFact);
 }
 
 let factFormElement = document.querySelector("#fact-form");
